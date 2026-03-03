71073e97726a4be36b0d29b0fd862cb8924e07d7
jasijnae <jasijnae@gmail.com>
2026-03-03 12:00:00 -0500

    Add Codewars solution

---
+++ sentence-smash.js
@@ -0,0 +1,18 @@
+function smash (words) {
+   return words.join(' ')
+   
+};
+
+/*S
+input
+  an arry
+  only words seperated by commas
+  may be uneven amount of spacing before or after
+output
+  one string no comma seperation
+tasks
+  remove commas
+  join words by comma
+    .join()
+  
+*/
